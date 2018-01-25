0046aa366adbd6a3e38dd8fa341f7bb13dc9fd5d
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2018-01-25 14:12:16 +0530

    Style scorelist page

--- js/app-scoreboard.js
+++ js/app-scoreboard.js
@@ -8,14 +8,14 @@ scoreboard.binder = function () {
         var detailPanel = $(this).next('.team-detail');
         var itemId = $(this).attr('id');
         if (!scoreboard.states[itemId]) {
-            detailPanel.slideDown();
+            detailPanel.stop().slideDown();
             scoreboard.states[itemId] = true;
             scoreboard.timeouts[itemId] = setTimeout(function () {
-                detailPanel.slideUp();
+                detailPanel.stop().slideUp();
                 scoreboard.states[itemId] = false;
             }, 4000)
         } else {
-            detailPanel.slideUp();
+            detailPanel.stop().slideUp();
             scoreboard.states[itemId] = false;
             clearTimeout(scoreboard.timeouts[itemId]);
         }
